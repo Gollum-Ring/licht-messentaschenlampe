af8e8526bcaba31540c40512f8408f585dd686f0
Gollum-Ring <Jakob.liss@gmail.com>
2025-03-24 19:38:52 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,17 @@
+let Licht = 0
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    Licht = input.lightLevel()
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    basic.showNumber(Licht)
+})
+input.onPinTouchEvent(TouchPin.P0, input.buttonEventDown(), function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.setLedColors(0xffffff, 0xffffff, 0xffffff)
+})
